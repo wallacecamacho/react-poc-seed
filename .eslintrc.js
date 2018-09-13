5541ca6ae50f0c13a03624e80cda95e14497dec1
Wallace C. Carlos <wallacecamacho@gmail.com>
2018-09-13 12:59:33 -0300

    config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/**
+ * React Static Boilerplate
+ * Copyright (c) 2015-present Kriasoft. All rights reserved.
+ */
+
+/* @flow 
+
+module.exports = {
+  extends: ['prettier'],
+  plugins: ['flowtype', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
+*/
+
+module.exports = {
+  extends : ["react-app"]
+}
